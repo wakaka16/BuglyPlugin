1dd3ef390acd23e855c57442577d282c9e53c303
wangxiaolong <582744719@qq.com>
2020-04-02 12:06:31 +0800

    修改buglyPlugin

--- www/BuglyPlugin.js
+++ www/BuglyPlugin.js
@@ -1,38 +1,38 @@
 var exec = require('cordova/exec');
 
 exports.initSDK = function(success, error, arg0) {
-    exec(success, error, "Bugly", "initSDK", [arg0]);
+    exec(success, error, "BuglyPlugin", "initSDK", [arg0]);
 };
 
 exports.enableJSMonitor = function() {
-    exec(null, null, "Bugly", "enableJSMonitor", []);
+    exec(null, null, "BuglyPlugin", "enableJSMonitor", []);
 };
 
 exports.setTagID = function(arg0) {
-    exec(null, null, "Bugly", "setTagID", [arg0]);
+    exec(null, null, "BuglyPlugin", "setTagID", [arg0]);
 };
 
 exports.setUserID = function(arg0) {
-    exec(null, null, "Bugly", "setUserID", [arg0]);
+    exec(null, null, "BuglyPlugin", "setUserID", [arg0]);
 };
 
 exports.putUserData = function(arg0) {
-    exec(null, null, "Bugly", "putUserData", [arg0]);
+    exec(null, null, "BuglyPlugin", "putUserData", [arg0]);
 };
 
 exports.testJavaCrash = function() {
-    exec(null, null, "Bugly", "testJavaCrash", []);
+    exec(null, null, "BuglyPlugin", "testJavaCrash", []);
 };
 
 exports.testNativeCrash = function() {
-    exec(null, null, "Bugly", "testNativeCrash", []);
+    exec(null, null, "BuglyPlugin", "testNativeCrash", []);
 };
 
 exports.testANRCrash = function() {
-    exec(null, null, "Bugly", "testANRCrash", []);
+    exec(null, null, "BuglyPlugin", "testANRCrash", []);
 };
 
 exports.testOCCrash = function() {
-    exec(null, null, "Bugly", "testOCCrash", []);
+    exec(null, null, "BuglyPlugin", "testOCCrash", []);
 };
 
